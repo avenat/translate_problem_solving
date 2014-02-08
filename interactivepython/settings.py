c358d47bc5e0df9c10e8a3078e4427a65f6ba37b
Ali Aliev <shamkir@gmail.com>
2014-02-08 22:49:59 +0400

    some changes

--- interactivepython/settings.py
+++ interactivepython/settings.py
@@ -52,7 +52,7 @@ STATICFILES_DIRS = (
 # Application definition
 
 INSTALLED_APPS = (
-    'bootstrap_admin',
+    'suit',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -78,17 +78,16 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-TEMPLATE_CONTEXT_PROCESSORS = (
-    "django.core.context_processors.request",
-    "django.contrib.auth.context_processors.auth",
-)
-
 ROOT_URLCONF = 'interactivepython.urls'
 
 WSGI_APPLICATION = 'interactivepython.wsgi.application'
 
 
 from django.conf.global_settings import TEMPLATE_CONTEXT_PROCESSORS as TCP
+
+TEMPLATE_CONTEXT_PROCESSORS = TCP + (
+    'django.core.context_processors.request',
+)
 
 
 # Internationalization
